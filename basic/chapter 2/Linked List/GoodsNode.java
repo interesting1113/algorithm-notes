3786b688aa0e9f624e76729efef8ca6718412c3b
interesting1113 <sylviayang1113@gmail.com>
2021-11-11 17:31:51 +0900

    单链表java

---
+++ basic/chapter 2/Linked List/GoodsNode.java
@@ -0,0 +1,15 @@
+package com.algorithm.linkedList;
+
+public class GoodsNode {
+
+    public int id;
+    public String name;
+    public double price;
+    public GoodsNode next;
+
+    public GoodsNode(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ basic/chapter 2/Linked List/LinkedList.java
@@ -0,0 +1,48 @@
+package com.algorithm.linkedList;
+
+public class LinkedList {
+
+    private GoodsNode node = new GoodsNode(0, "", 0.0);
+
+    /**
+     * 往链表中尾部添加节点
+     * @param goodsNode
+     */
+    public void add(GoodsNode goodsNode) {
+        GoodsNode temp = node;
+        while (true) {
+            if (temp.next == null) {
+                break;
+            }
+            temp = temp.next;
+        }
+        temp.next = goodsNode;
+    }
+
+    /**
+     * 按从小到大顺序进行添加
+     * @param goodsNode
+     */
+    public void addOrder(GoodsNode goodsNode) {
+        GoodsNode temp = node;
+        boolean flag = false;
+        while (true) {
+            if (temp.next == null) {
+                break;
+            }
+            if (temp.next.id > goodsNode.id) {
+                break;
+            } else if (temp.next.id == goodsNode.id){
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            System.out.println("已经存在该商品，不能添加重复商品");
+        } else {
+            goodsNode.next = temp.next;
+            temp.next = goodsNode;
+        }
+    }
+}
