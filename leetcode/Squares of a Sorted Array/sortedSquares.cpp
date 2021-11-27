09fcaefaa792af670ab0e3ea08fa77fe5507e721
interesting1113 <sylviayang1113@gmail.com>
2021-11-27 16:37:03 +0900

    Squares of a Sorted Array

---
+++ leetcode/Squares of a Sorted Array/sortedSquares.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        int i = 0, j = n - 1, k = n - 1;
+        while (i <= j) {
+            if (nums[i] * nums[i] > nums[j] * nums[j]) {
+                res[k] = nums[i] * nums[i];
+                i ++, k --;
+            } else {
+                res[k] = nums[j] * nums[j];
+                j --, k --;
+            }
+        }
+        return res;
+    }
+};
