8fe0ceae9ca56033e926487a1998be87114b01ef
interesting1113 <sylviayang1113@gmail.com>
2022-03-27 13:44:31 +0900

    从尾到头打印链表

---
+++ 剑指offer/question5.cpp
@@ -0,0 +1,21 @@
+// 从尾到头打印链表
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> printListReversingly(ListNode* head) {
+        vector<int> res;
+        while(head) {
+            res.push_back(head->val);
+            head = head->next;
+        }
+        return vector<int> (res.rbegin(), res.rend());
+    }
+};
