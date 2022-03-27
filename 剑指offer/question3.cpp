ddaeb7efad5c7fbcd5d61d325fb1fda4c6016f1a
interesting1113 <sylviayang1113@gmail.com>
2022-03-27 11:19:12 +0900

    二维数组中的查找

---
+++ 剑指offer/question3.cpp
@@ -0,0 +1,16 @@
+// 二维数组中的查找
+class Solution {
+public:
+    bool searchArray(vector<vector<int>> array, int target) {
+        if (array.empty() || array[0].empty()) return false;
+        int i = 0, j = array[0].size() - 1;
+        while (i < array[0].size() && j >= 0) {
+            int x = array[i][j];
+            if (x == target) return true;
+            if (x > target) j --;
+            else i ++;
+        }
+
+        return false;
+    }
+};
