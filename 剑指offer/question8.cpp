efe47e4dd951e54334dcba9735680bcd00886d33
interesting1113 <sylviayang1113@gmail.com>
2022-03-30 22:48:13 +0900

    剪绳子

---
+++ 剑指offer/question8.cpp
@@ -0,0 +1,15 @@
+// 剪绳子
+class Solution {
+public:
+    int maxProductAfterCutting(int length) {
+        if (length <= 3) return 1 * (length - 1);
+
+        int res = 1;
+        if (length % 3 == 1) res *= 4, length -= 4;
+        if (length % 3 == 2) res *= 2, length -= 2;
+
+        while (length) res *= 3, length -= 3;
+
+        return res;
+    }
+};
