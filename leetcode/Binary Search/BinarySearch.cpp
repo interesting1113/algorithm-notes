19aa5b47258f08bc07ba2d5a2804773a15889ddb
interesting1113 <sylviayang1113@gmail.com>
2022-04-03 11:54:01 +0900

    update binary search

--- leetcode/Binary Search/BinarySearch.cpp
+++ leetcode/Binary Search/BinarySearch.cpp
@@ -3,7 +3,9 @@ public:
     int search(vector<int>& nums, int target) {
         int l = 0, r = nums.size() - 1;
         while (l < r) {
-            int mid = l + r >> 1;
+            // l + r 逐步操作可能会导致int溢出
+            // int mid = (long long)l + r >> 1;
+            int mid = l + (r - l) / 2;
             if (nums[mid] >= target) r = mid;
             else l = mid + 1;
         }
