34258f7a074cb04bd4f8c69e7593214f439b1c29
interesting1113 <sylviayang1113@gmail.com>
2022-04-04 17:08:45 +0900

    Peak Index in a Mountain Array

---
+++ leetcode/Peak Index in a Mountain Array/Peak IndexinaMountainArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 1, r = arr.size() - 2;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (arr[mid] > arr[mid - 1]) l = mid;
+            else r = mid - 1;
+        }
+
+        return l;
+    }
+};
