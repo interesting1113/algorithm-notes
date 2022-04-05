45174ff411410eaa34de2898497c58493f77a703
interesting1113 <sylviayang1113@gmail.com>
2022-04-05 17:35:50 +0900

    update move zeroes

--- leetcode/Move Zeroes/MoveZeroes.cpp
+++ leetcode/Move Zeroes/MoveZeroes.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // k表示第二个指针
         int k = 0;
         for (int i = 0; i < nums.size(); i ++) {
+            // 当i位置的值不是零的时候，将i位置的值放到k指向的位置，然后k向后移动
             if (nums[i] != 0) {
                 nums[k ++] = nums[i];
             }
         }
-
+        // 在后边补上零
         while (k < nums.size()) nums[k ++] = 0;
     }
 };
