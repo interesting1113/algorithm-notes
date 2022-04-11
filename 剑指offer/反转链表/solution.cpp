1b2900318c7348b155b6e0ed78802b686bc1fdd7
interesting1113 <sylviayang1113@gmail.com>
2022-04-11 22:01:38 +0900

    合并两个排序的链表

--- 剑指offer/反转链表/solution.cpp
+++ 剑指offer/反转链表/solution.cpp
@@ -9,13 +9,13 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* pre = nullptr;
-        auto cur = head;
+        ListNode* pre = nullptr; // 定义一个空指针
+        auto cur = head; // 把头节点存到当前节点
         while (cur) {
-            auto next = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = next;
+            auto next = cur->next; //因为下面的操作会改变当前节点的下一个节点，所以需要先存下
+            cur->next = pre; //让当前节点的下一个指针往回指
+            pre = cur; //pre存下当前节点，让后面的节点指向pre
+            cur = next; // 移动当前节点
         }
         return pre;
     }
