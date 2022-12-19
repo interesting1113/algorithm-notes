9a74b6686602b96c7eebd41fe7140f3bbf4ceed8
interesting1113 <sylviayang1113@gmail.com>
2022-12-19 20:28:03 +0900

    reverse linked list

--- basic/chapter2/Linked List/linkedList.cpp
+++ basic/chapter2/Linked List/linkedList.cpp
@@ -96,6 +96,34 @@ void display(Node *head)
   cout << "NULL" << endl;
 }
 
+Node* reverse(Node* &head) {
+  Node* prevPtr = NULL;
+  Node* currPtr = head;
+  Node* nextPtr;
+
+  while(currPtr != NULL) {
+    nextPtr = currPtr->next;
+    currPtr->next = prevPtr;
+
+    prevPtr = currPtr;
+    currPtr = nextPtr;
+  }
+
+  return prevPtr;
+}
+
+Node* reverseRecursive(Node* &head) {
+  if (head == NULL || head->next == NULL) {
+    return head;
+  }
+
+  Node* newHead = reverseRecursive(head->next);
+  head->next->next = head;
+  head->next = NULL;
+
+  return newHead;
+}
+
 int main()
 {
   Node *head = NULL;
